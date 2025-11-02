85aeb21a820d1395e58754602c844c7754ea49e0
felix <fel93@live.fr>
2025-11-02 23:19:56 +0100

    fix(packages): uninstall useless package

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { QueryClient, QueryClientProvider, dehydrate } from '@tanstack/react-query';
 import { HydrationBoundary } from '@tanstack/react-query';
-import { StaticRouter } from 'react-router-dom';
+import { StaticRouter } from 'react-router';
 import HomePage from './pages/HomePage';
 
 export async function render(url: string) {
